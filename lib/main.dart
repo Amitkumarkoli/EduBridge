eaee7b54e6ff5c70146cfa7f18f3e90404c42bbb
Amitkumarkoli <akdev523@gmail.com>
2025-05-26 18:35:59 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'core/providers/navigation_provider.dart';
-// import 'core/providers/theme_provider.dart';
-// import 'core/providers/language_provider.dart';
-// import 'routes/app_routes.dart';
-// import 'theme.dart/theme.dart';
-// import 'package:flutter_localizations/flutter_localizations.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//       providers: [
-//         ChangeNotifierProvider(create: (_) => NavigationProvider()),
-//         ChangeNotifierProvider(create: (_) => ThemeProvider()),
-//         ChangeNotifierProvider(create: (_) => LanguageProvider()),
-//       ],
-//       child: Consumer<LanguageProvider>(
-//         builder: (context, languageProvider, child) {
-//           return MaterialApp(
-//             debugShowCheckedModeBanner: false,
-//             theme: AppTheme.lightTheme,
-//             locale: languageProvider.currentLocale,
-//             supportedLocales: const [
-//               Locale('en'),
-//               Locale('hi'),
-//             ],
-//             localizationsDelegates: const [
-//               GlobalMaterialLocalizations.delegate,
-//               GlobalWidgetsLocalizations.delegate,
-//               GlobalCupertinoLocalizations.delegate,
-//             ],
-//             initialRoute: '/',
-//             routes: AppRoutes.routes,
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'core/providers/language_provider.dart';
@@ -99,7 +13,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
